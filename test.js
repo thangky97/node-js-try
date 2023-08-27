ebfa7eae9e7ed0ab5320f8af2a8e61e18cfde7be
thangtv <tranthangvui360@gmail.com>
2023-08-27 20:54:03 +0700

    create mongodb

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
-console.log("testttt");
+// var mongodb = require('mongodb').MongoClient;
+
+// var url = 'mongodb+srv://admin:admin123@testdb.iqt6lky.mongodb.net/?retryWrites=true&w=majority';
+
+// mongodb.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = data.db("mydb");
+//     dbo.createCollection("Customers", function(err, res) {
+//       if (err) throw err;
+//       console.log("Collection created!");
+//       data.close();
+//     });
+//   });
+
+var MongoClient = require("mongodb").MongoClient;
+var url =
+  "mongodb+srv://admin:admin123@testdb.iqt6lky.mongodb.net/?retryWrites=true&w=majority";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function (err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
